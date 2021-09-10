8b10aa5a0d980e79deabaa1e303384f4df46c7f8
Mohammed Rezq <mohammedrezq2000@gmail.com>
2021-09-10 23:07:10 +0200

    [DARK-MODE]

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -13,6 +13,7 @@ import styles from './Nav.module.scss';
 import Dropdown from 'components/Dropdown';
 import NavMenu from 'components/NavMenu/NavMenu';
 import NavMenuMobile from 'components/NavMenu/NavMenuMobile';
+import DarkModeToggle from 'components/DarkMode/DarkModeToggle';
 
 const SEARCH_VISIBLE = 'visible';
 const SEARCH_HIDDEN = 'hidden';
@@ -191,6 +192,7 @@ const Nav = () => {
         </p>
         <NavMenu />
         <NavMenuMobile />
+        <DarkModeToggle />
         <div className={styles.navSearch}>
           {searchVisibility === SEARCH_HIDDEN && (
             <button onClick={handleOnToggleSearch} disabled={!searchIsLoaded}>
