d1e4b8a0909b87cd662288a5b68113c7462fdff9
Mohammed Rezq <mohammedrezq2000@gmail.com>
2021-09-11 02:03:29 +0200

    [Routes] Comment out i18 object

--- next.config.js
+++ next.config.js
@@ -30,24 +30,24 @@ module.exports = withPlugins([[indexSearch], [feed], [sitemap], [socialImages]],
   images:{
     domains:['localhost', '0.gravatar.com', 'gravatar.com'],
   },
-  i18n: {
-    // These are all the locales you want to support in
-    // your application
-    locales: ['ar', 'en-US'],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: 'ar',
-    // This is a list of locale domains and the default locale they
-    // should handle (these are only required when setting up domain routing)
-    // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
-    localeDetection: true,
-    domains: [
-      {
-        domain: 'http://localhost/',
-        defaultLocale: 'ar',
-      },
-    ],
-  },
+  // i18n: {
+  //   // These are all the locales you want to support in
+  //   // your application
+  //   locales: ['ar', 'en-US'],
+  //   // This is the default locale you want to be used when visiting
+  //   // a non-locale prefixed path e.g. `/hello`
+  //   defaultLocale: 'ar',
+  //   // This is a list of locale domains and the default locale they
+  //   // should handle (these are only required when setting up domain routing)
+  //   // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
+  //   localeDetection: true,
+  //   domains: [
+  //     {
+  //       domain: 'http://localhost/',
+  //       defaultLocale: '/',
+  //     },
+  //   ],
+  // },
 });
 
 /**
